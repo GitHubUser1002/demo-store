0e99ec2a50bce617ef0cfc3688cc018fa257a443
Brian Do <bdo@squarespace.com>
2019-07-12 13:17:42 -0400

    Forgot some files last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './App.css'
+import { StickyContainer, Sticky } from 'react-sticky';
+import * as _ from 'lodash';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Products from './Products';
+import Product from './Product';
+import { ReactComponent as Icon } from './icons/cart.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StickyContainer>
+      <Router>
+      <Route path="/" render={() => (
+        <Sticky>
+          {({ style }) => (<header className="App-header" style={style}>
+            <div/>
+            <div className="Center-Header"><Link className="HomeLink" to="/">Store</Link></div>
+            <div className="Cart-Header"><Icon className="Cart" width={33}/></div>
+          </header>)}
+        </Sticky>)
+      } />
+        <Route path='/product/:productId' render={({match}) => <Product productId={match.params.productId} />}/>
+        <Route exact={true} path='/' component={Products}/>
+      </Router>
+    </StickyContainer>
   );
 }
 
